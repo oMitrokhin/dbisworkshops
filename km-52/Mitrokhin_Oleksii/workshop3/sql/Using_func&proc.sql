2a744f451ed64683545fb8af1b43699394681eea
oMitrokhin <mitkamav@gmail.com>
2018-12-25 22:54:56 +0200

    Update Using_func&proc.sql

--- km-52/Mitrokhin_Oleksii/workshop3/sql/Using_func&proc.sql
+++ km-52/Mitrokhin_Oleksii/workshop3/sql/Using_func&proc.sql
@@ -4,5 +4,10 @@ P_user_product.add_product_to_userbase('user@email.com','Fender Classic Shell Me
 P_user_product.delete_user_product('user@email.com','Fender Classic Shell Medium Guitar Pick', '03-DEC-18'); 
 P_user_product.edit_product_in_userbase('mail_51@email.com', 'Dunlop Tortex 0.73mm Guitar Pick', to_date('2018-12-3', 'YYYY-MM-DD'), '1', '15', 'High'); 
 dbms_output.put_line(P_user_product.get_user_product('mail_51@email.com')); 
-
+                                                     
+ for rec in (select * from table(P_Av_PR.get_available_product()))
+    loop
+        DBMS_OUTPUT.put_line(rec.product_name || ' ' || rec.product_average_price);
+    end loop;
+                                 
 end;
